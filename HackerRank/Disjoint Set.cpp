4abad82a732cbd088f401136779b484c2968d41d
ShiviAgrawal113114 <agrawalshivi113114@gmail.com>
2020-05-24 20:50:11 +0530

    code

---
+++ HackerRank/Disjoint Set.cpp
@@ -0,0 +1,29 @@
+/* https://www.hackerrank.com/contests/all-india-contest-by-coding-club-india/challenges/colony-unification */
+
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 1e5+100;
+int parent[N];
+int findParent(int x){
+    if(parent[x] != x)parent[x] = findParent(parent[x]);
+    return parent[x];
+}
+
+void join(int a, int b){
+    parent[findParent(a)] = findParent(b);
+}
+
+int main(){
+    int N,M,a,b;
+    scanf("%d%d",&N,&M);
+    for(int i = 1; i <= N; ++i)parent[i] = i;
+    while(M--){
+        scanf("%d%d",&a,&b);
+        if(findParent(a) != findParent(b)){
+            join(a,b);
+            N--;
+        }
+        printf("%d\n",N);
+    } 
+    return 0;
+}
