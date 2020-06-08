a14fe6b1b40816690275703ddd3d5fa6780fcd14
Shivi Agrawal <shiviagrawal98@gmail.com>
2020-06-09 00:55:21 +0530

    June Challenge

---
+++ LeetCode/June Challenge/Coin Change 2.cpp
@@ -0,0 +1,26 @@
+/* https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3353/ */
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1));
+        int i,j;
+        for(i=1;i<=amount; i++)
+            dp[0][i] = 0;
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+        
+        for(int i=1; i<=n; i++)
+        {
+            for(int j = 1; j<=amount; j++)
+            {
+                if(j>=coins[i-1])
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][amount];
+    }
+};
